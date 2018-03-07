4c847e8b2d50c386887e2e9726c14cc568e145a0
information <01678424113b@gmail.com>
2018-03-07 23:32:12 +0700

    done

--- getLastLink.php
+++ getLastLink.php
@@ -1,15 +1,24 @@
 <?php
 function getLastLink($url)
 {
-    $header = get_headers($url);
+    $header = get_headers('https://www.collectoffers.com/th'.$url);
     $header = implode(" ", $header);
-    $location = preg_match('/Location:\s(.*?)\sCache-Control/', $header, $result);
-    $last_link = '';
-    if(count($result) > 0){
-        $last_link = $result[1];
+    $location = preg_match('/Location\:.*?\&url=(.*?)\s/', $header, $result);
+    if(count($result) == 0){
+        $location = preg_match('/Location\:.*?\&r=(.*?)\s/', $header, $result);
     }
+    $last_link = $result[1];
+    $last_link = str_replace("%3A", ":", $last_link);
+    $last_link = str_replace("%3a", ":", $last_link);
+    $last_link = str_replace("%2F", "/", $last_link);
+    $last_link = str_replace("%2f", "/", $last_link);
     return $last_link;
 }
+
 ?>
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script>
+
+</script>
 
